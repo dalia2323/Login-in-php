a9c148fb977759fe8f27ea5f50b3584a61cbb1da
dalia2323 <d.k.saleem@students.ptuk.edu.ps>
2023-02-23 15:49:05 +0200

    validation

--- handel.php
+++ handel.php
@@ -2,6 +2,25 @@
 session_start();
 $email=$_POST['email'];
 $password=$_POST['password'];
+$error[];
+$passwordLen=strlen($password);
+//VALIDATE EMAIL
+if(empty($email)){
+    $errors[]="email is required";  
+}
+else if (Filter_var(!$email,FILTER_VALIDATE_EMAIL)){
+    $errors[]="Email is not valid";
+
+}
+//VALIDATE PASSWORD 
+if (empty($password))
+{
+$errors[]="password is required";    
+}
+elseif($passwordLen<8 && $passwordLen>30){
+    $errors[]="password should be 8 to 30 charecter";
+}
+if (empty(errors[])){
 $dbemail="dalia@gmail.com";
 $dbpassword="123";
 if($email==$dbemail)
@@ -19,5 +38,5 @@ if($email==$dbemail)
 else{
     $_SESSION['error']="Invalid email";
     header("location:index.php");
-}
+}}
 ?>
